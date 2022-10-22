0c97e45e92414082556f73ea6b7ee09c0b7313e9
Mohamed Shaban <57798592+mohamedshabban@users.noreply.github.com>
2022-10-22 17:23:28 +0200

    Main Branch

---
+++ DelegateByMosh/DelegateByMosh/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace DelegateByMosh
+{
+    public class Photo
+    {
+        public static Photo Load(string path)
+        {
+            return new Photo();
+        }
+        public void Save()
+        {
+
+        }
+    }
+    public class PhotoProcessor
+    {
+        public delegate void PhotoFilterHandler(Photo photo);
+
+        public void Process(string path ,PhotoFilterHandler photoFilterHandler)
+        {
+            var photo = Photo.Load(path);
+            photoFilterHandler(photo);
+            photo.Save();
+        }
+    }
+    public class PhotoFilter
+    {
+        public void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("Apply photo brightness");
+        }
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("Apply photo contrast");
+
+        }
+        public void Resize(Photo photo)
+        {
+            Console.WriteLine("Resize photo");
+
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Action<> // pointer to a method that return void
+
+            //System.Func<> // points to a method that return value 
+
+            //Predicate<> //Special case from func that return bool if object meets the criteria
+
+            var photoFilter = new PhotoFilter();
+
+            PhotoProcessor.PhotoFilterHandler photoFilterHandler = photoFilter.ApplyBrightness;
+            photoFilterHandler += photoFilter.ApplyContrast;
+            photoFilterHandler += RemoveNoises;
+
+
+            var photoProcessor = new PhotoProcessor();
+            photoProcessor.Process("image.png", photoFilterHandler);
+
+        }
+        static void RemoveNoises(Photo photo)
+        {
+            Console.WriteLine("Remove noises of photo");
+        }
+    }
+}
